10a2b3810b6f0e129803e07cd5ec394f37e94e53
klimslava <eddy.kiselman@gmail.com>
2016-03-24 14:01:27 +0200

    Added questions migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319210205) do
+ActiveRecord::Schema.define(version: 20160324113521) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -30,6 +36,19 @@ ActiveRecord::Schema.define(version: 20160319210205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title",                   null: false
+    t.string   "summary"
+    t.string   "content",                 null: false
+    t.string   "video_link"
+    t.integer  "category_id", default: 1, null: false
+    t.integer  "founder_id"
+    t.integer  "mentor_id"
+    t.integer  "vote"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
